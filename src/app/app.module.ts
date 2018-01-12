78aea76f096e662ebc26fd61581e9c228c9615e4
Ilija Zubac <ilija.zubac@gmail.com>
2018-01-12 20:05:23 +0100

    2 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { BuyersComponent } from './components/buyers/buyers.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
+const appRoutes: Routes = [
+  {path: '', component: ProductsComponent },
+  { path: 'buyers', component: BuyersComponent },
+  { path: 'buyers/:id', component: BuyersComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'products/:id', component: ProductsComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    BuyersComponent,
+    ProductsComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
